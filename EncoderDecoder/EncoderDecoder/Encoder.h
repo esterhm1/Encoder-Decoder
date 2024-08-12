eb162d4798ba191bba0c9fb06e919cb106c7fa9f
esterhm1 <e0583280913@gmail.com>
2024-08-12 13:11:26 +0300

    code review

---
+++ EncoderDecoder/EncoderDecoder/Encoder.h
@@ -0,0 +1,30 @@
+#pragma once
+#define BLOCK_SIZE 8
+#define HAMMING1_SIZE 4
+#define HAMMING2_SIZE 3
+
+#pragma pack(push, 1)
+
+typedef struct ProtectionData {
+	unsigned char parityBit : 1;
+	unsigned int hamming1 : HAMMING1_SIZE;
+	unsigned int hamming2 : HAMMING2_SIZE;
+}ProtectionData;
+
+#pragma pack(pop)
+
+int MultyXor(void* data);
+int calculateNumberOfParityBitsForBlock(int block_length);
+int isPowerOfTwo(size_t num);
+int getAdjustedIndex(int index, int skipped);
+int getBitValue(const void* data, int bitPosition);
+void binaryRepresent(void* data, int bitsNumber);
+void printBinary(unsigned int num);
+int hammingEncode(const void* data, int skipped);
+void blockEncode(void* block, char* protectionFileName);
+void encode(char* data, long length);
+void readProtectionData();
+
+
+
+
--- EncoderDecoder/EncoderDecoder/encoder.c
+++ EncoderDecoder/EncoderDecoder/encoder.c
@@ -2,117 +2,100 @@
 #include<stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "Encoder.h"
 #define _CRT_SECURE_NO_WARNINGS
-#define BLOCK_SIZE 8
-#define HAMMING1_SIZE 4
-#define HAMMING2_SIZE 3
 
-
-
-#pragma pack(push, 1)
-
-typedef struct ProtectionData {
-	unsigned char parityBit : 1;
-	unsigned int hamming1 : HAMMING1_SIZE;
-	unsigned int hamming2 : HAMMING2_SIZE;
-}ProtectionData;
-
-#pragma pack(pop)
-
-
-
-int MultyXor(void* data, size_t len) {
-	unsigned char* byte_ptr = (unsigned char*)data;
+int MultyXor(void* data) {
+	unsigned char* bytePtr = (unsigned char*)data;
 	int parity = 0;
 
-	for (size_t i = 0; i < len; ++i) {
-		unsigned char byte = byte_ptr[i];
+	for (size_t i = 0; i < BLOCK_SIZE; ++i) {
+		unsigned char byte = bytePtr[i];
 
 		for (int bit = 0; bit < 8; ++bit) {
 
-			uint8_t bit_value = (byte >> bit) & 1;
+			uint8_t bitValue = (byte >> bit) & 1;
 
-			parity ^= bit_value;
+			parity ^= bitValue;
 		}
 	}
 	return parity;
 }
 
-int calculate_number_of_parity_bits_for_block(int block_length)
-{
-	int count = 0;
-	while ((1 << count) < (block_length + count + 1)) {
-		count++;
+int calculateNumberOfParityBitsForBlock(int block_length){
+	int counter = 0;
+	while ((1 << counter) < (block_length + counter + 1)) {
+		counter++;
 	}
 
-	return count;
+	return counter;
 }
 
-int is_power_of_two(size_t num) {
+int isPowerOfTwo(size_t num) {
 	return (num & (num - 1)) == 0;
 }
 
-int get_adjusted_index(int index, int skipped) {
-	// אם האינדקס הוא חזקה של 2, החזר -1
-	if (is_power_of_two(index)) {
+int getAdjustedIndex(int index, int skipped) {
+	// If the index is a power of 2, return -1.
+	if (isPowerOfTwo(index)) {
 		return -1;
 	}
 
-	// אחרת, חשב את האינדקס המתואם
-	int adjusted_index = 1;
+	// Otherwise, calculate the adjusted index.
+	int adjustedIndex = 1;
 	for (size_t i = 1; i < index; ++i) {
-		if (!is_power_of_two(i)) {
-			++adjusted_index;
+		if (!isPowerOfTwo(i)) {
+			++adjustedIndex;
 		}
 	}
 	if (skipped == 1)
-		return adjusted_index;
-	return adjusted_index * 2 - 1;
+		return adjustedIndex;
+	return adjustedIndex * 2 - 1;
 }
 
-int get_bit_value(const void* data, int bitPosition) {
+int getBitValue(const void* data, int bitPosition) {
 	bitPosition--;
-	// המרת המצביע ל-unsigned char* כדי לגשת לנתונים כבתים
-	const unsigned char* byte_ptr = (const unsigned char*)data;
+	// Pointer conversion to unsigned char*to access the data as bytes
+	const unsigned char* bytePtr = (const unsigned char*) data;
 
-	// חישוב מיקום ה-byte והביט בתוך ה-byte
+	// Calculate the position of the byte and the bit within the byte.
 	int byteIndex = (bitPosition) / 8;
 	int bitIndex = 7 - ((bitPosition) % 8); // 7 - bitIndex כדי לקרוא ביטים משמאל לימין
 
-	// נוודא שמיקום ה-byte הוא בתוך הגבולות של האזור הזיכרון
-	// (נניח שהאזור זיכרון הוא קטן ממערך של unsigned char)
+	// Verify the location of the byte is within the boundaries of the memory area
+	// (Suppose the memory area is smaller than an array of unsigned char)
 	if (byteIndex < 0 || byteIndex >= sizeof(data)) {
-		return -1; // שגיאה: מיקום מחוץ לטווח
+		return -1; // Error: Position out of range
 	}
 
-	// הוצאת הבית המתאים מהמצביע
-	unsigned char byte = byte_ptr[byteIndex];
+	// Removing the appropriate byte from the pointer
+	unsigned char byte = bytePtr[byteIndex];
 
-	// יצירת מסכה לבדוק את ערך הביט המתאים
+	// Create a mask to check the corresponding bit value
 	unsigned char mask = 1 << (bitIndex);
 
-	// שימוש במסכה כדי לבדוק את ערך הביט המתאים
+	// Using a mask to check the corresponding bit value
 	int bitValue = (byte & mask) >> bitIndex;
 
 	return bitValue;
 }
 
-void binary_represent(void* data, int bits_number) {
-	// מצביע לנתונים כ-unsigned char
-	unsigned char* byte_ptr = (unsigned char*)data;
+void binaryRepresent(void* data, int bitsNumber) {
+	// Pointer to the data as unsigned char
+	unsigned char* bytePtr = (unsigned char*) data;
 
-	// חישוב מספר הבתים הכולל
-	int total_bytes = (bits_number + 7) / 8; // Round up to include all bytes
+	// Calculation of the total number of bytes
+	int totalBytes = (bitsNumber + 7) / 8; // Round up to include all bytes
 
-	// מדפיס את הסיביות
-	for (int i = 0; i < total_bytes; ++i) {
-		unsigned char byte = byte_ptr[i];
+	// Prints the bits
+	for (int i = 0; i < totalBytes; ++i) {
+		unsigned char byte = bytePtr[i];
 
-		// נוודא שאנחנו לא מדפיסים יותר סיביות מהנתון
-		int bits_to_print = (i == total_bytes - 1) ? (bits_number % 8) : 8;
-		if (bits_to_print == 0) bits_to_print = 8; // אם זה byte הראשון
+		// Ensure that no more bits are printed than the given data.
+		int bitsToPrint = (i == totalBytes - 1) ? (bitsNumber % 8) : 8;
+		if (bitsToPrint == 0) bitsToPrint = 8; // If it's the first byte 
 
-		for (int bit = bits_to_print - 1; bit >= 0; --bit) {
+		for (int bit = bitsToPrint - 1; bit >= 0; --bit) {
 			printf("%d ", (byte >> bit) & 1);
 		}
 	}
@@ -120,44 +103,42 @@ void binary_represent(void* data, int bits_number) {
 	printf("\n");
 }
 
-void print_binary(unsigned int num) {
-	const int BITS = sizeof(num) * 8;  // מספר הביטים ב-unsigned int
+void printBinary(unsigned int num) {
+	const int BITS = sizeof(num) * 8;  // The number of bits in unsigned int
 
-	// נשתמש בלולאה להדפיס את כל הביטים מהכי גבוה לנמוך ביותר
+	// Print the bit from the MSB to the LSB
 	for (int i = BITS - 1; i >= 0; --i) {
-		// הדפס את הביט המתאים
 		printf("%d", (num >> i) & 1);
 
-		// הוסף רווח אחרי כל 8 ביטים
+		// Space after 8 bits
 		if (i % 8 == 0 && i != 0) {
 			printf(" ");
 		}
 	}
 
-	// הדפס שורה חדשה בסוף
 	printf("\n");
 }
 
 
-uint64_t hamming_encode(const void* data, size_t len, int skipped) {
-	int number_of_parity_bits = calculate_number_of_parity_bits_for_block(len / skipped);
-	uint64_t result = 0;
+int hammingEncode(const void* data, int skipped) {
+	int numberOfParityBits = calculateNumberOfParityBitsForBlock(BLOCK_SIZE / skipped);
+	int result = 0;
 
 	// Compute and store the parity bits
-	for (int i = 1; i <= number_of_parity_bits; i++) {
-		int this_parity_bit = 0;
+	for (int i = 1; i <= numberOfParityBits; i++) {
+		int thisParityBit = 0;
 
-		for (int j = (int)pow(2, i - 1); j <= len / skipped + number_of_parity_bits; j++) {
-			int index = get_adjusted_index(j, skipped);
+		for (int j = (int)pow(2, i - 1); j <= BLOCK_SIZE / skipped + numberOfParityBits; j++) {
+			int index = getAdjustedIndex(j, skipped);
 
 			if (index != -1 && (j & (int)pow(2, i - 1))) {
-				this_parity_bit ^= get_bit_value(data, index);
+				thisParityBit ^= getBitValue(data, index);
 			}
 		}
 
 		// Store the parity bit in the result
-		if (this_parity_bit) {
-			result |= (1ULL << (number_of_parity_bits - i));
+		if (thisParityBit) {
+			result |= (1ULL << (numberOfParityBits - i));
 		}
 	}
 
@@ -165,34 +146,30 @@ uint64_t hamming_encode(const void* data, size_t len, int skipped) {
 }
 
 
-void block_encode(void* block, char* protection_file_name)
-{
+void blockEncode(void* block, char* protectionFileName){
 	ProtectionData protection;
-	protection.parityBit = MultyXor(block, BLOCK_SIZE);
-	protection.hamming1 = hamming_encode(block, BLOCK_SIZE, 1);
-	protection.hamming2 = hamming_encode(block, BLOCK_SIZE, 2);
+	protection.parityBit = MultyXor(block);
+	protection.hamming1 = hammingEncode(block, 1);
+	protection.hamming2 = hammingEncode(block, 2);
 	FILE* file = NULL;
-	fopen_s(&file, protection_file_name, "ab");
+	fopen_s(&file, protectionFileName, "ab");
 	fwrite(&protection, sizeof(protection), 1, file);
 	fclose(file);
 }
 
-void encode(void* data, long length)
-{
-	char* protection_file_name = "output.bin";
+void encode(char* data, long length){
+	char* protectionFileName = "output.bin";
 	void* ptr;
 	int index = 0;
 	while (index + BLOCK_SIZE <= length)
 	{
-		block_encode(data, protection_file_name);
+		blockEncode(data, protectionFileName);
 		index += BLOCK_SIZE;
+		data += BLOCK_SIZE / 8;
 	}
-
-
-
 }
 
-void read_protection_data() {
+void readProtectionData() {
 	FILE* file = NULL;
 	fopen_s(&file, "output.bin", "rb"); // Read mode
 	if (file == NULL) {
@@ -208,22 +185,10 @@ void read_protection_data() {
 		ProtectionData d;
 		fread(&d, sizeof(ProtectionData), 1, file);
 		printf("%d\n", d.parityBit);
-		print_binary(d.hamming1);
-		print_binary(d.hamming2);
+		printBinary(d.hamming1);
+		printBinary(d.hamming2);
 
 	}
 
 	fclose(file);
-}
-
-
-
-int main()
-{
-	int data = 3;
-	encode(&data, sizeof(data) * 8);
-
-
-	//רק לוודא שהכל פועל כשורה
-	read_protection_data();
 }
